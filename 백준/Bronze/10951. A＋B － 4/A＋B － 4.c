bc6fbfc273ade80b34d9643a44a0fa9f24470ed2
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-03-16 21:47:17 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+
+	while (scanf("%d %d", &a, &b) != EOF) {
+		printf("%d\n", a + b);
+	}
+
+	return 0;
+}
