73d28d4ce37eb45951cbaca788aa2306df6b8053
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-04-28 22:16:54 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	for (int i = n; i >= 1; i--) {
+		for (int j = 0; j < i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
