e7d572a5b4b3d1b4e9eaa09a3d2f6113395fde38
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-05-02 10:58:08 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.c
+++ 백준/Bronze/15829. Hashing/Hashing.c
@@ -1,20 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 
 int main() {
 	long long int total = 0;
 	int n;
-	scanf("%s", &n);
+	scanf("%d", &n);
 
 	char str[51];
 	scanf("%s", str);
 
-	for (int i = 0; i < strlen(str); i++) {
-		total += (str[i] - 96) * pow(31, i);
+	for (int i = 0; i < n; i++) {
+		long long int m = 1;
+		for (int j = 0; j < i; j++) {
+			m *= 31;
+			m %= 1234567891;
+		}
+		total += ((str[i] - 96) * m) % 1234567891;
+		total %= 1234567891;
 	} // 총 값 += (ASCII (int)str - 96) -> a = 1, b = 2, ... * 31^i
+	// 에다가 mod 1234567891
 
-	printf("%lld", total);
+	printf("%lld", total % 1234567891);
 	return 0;
 }
