5486f13d44d73b875c64b87167e9b88b06997d3d
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-05-07 23:56:12 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	int arr[1010];
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	} // 버블 정렬
+
+	for (int i = 0; i < n; i++) {
+		printf("%d\n", arr[i]);
+	}
+
+	return 0;
+}
