15ca3b6d32ebdd588d5c4f22883466716e507814
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-05-09 11:16:34 +0900

    [Bronze V] Title: A+B - 9, Time: 4 ms, Memory: 1124 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15740. A＋B － 9/A＋B － 9.c
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.c
@@ -25,18 +25,18 @@ int main() {
 
 		// 첫 문자가 - 이면 음수 그룹에 더하기
 		if (str[0] == '-') {
-			for (int k = 1; k < strlen(str); k++) {
+			for (int k = 1; str[k] != NULL; k++) {
 				Negative.arr[strlen(str) - k - 1] += str[k] - '0';
 			}
 		} // 아니면 양수 그룹에 더하기
 		else {
-			for (int k = 0; k < strlen(str); k++) {
+			for (int k = 0; str[k] != NULL; k++) {
 				Positive.arr[strlen(str) - k - 1] += str[k] - '0';
 			}
 		}
 
 		// 10 진법 이므로 각 자리 수에 해당하는 배열 원소가 10을 넘으면 캐리 보내기
-		for (int i = 0; i < strlen(str); i++) {
+		for (int i = 0; i < 10100; i++) {
 			if (Positive.arr[i] / 10 > 0) {
 				Positive.arr[i + 1] += (Positive.arr[i] / 10);
 				Positive.arr[i] %= 10;
@@ -51,7 +51,7 @@ int main() {
 	// same은 boolean, 10001번 (제일 높은 자리) 부터 0번 자리까지 검사 후 != 이면 same = 0 반환
 	int same = 1;
 	char integer;
-	for (int i = 10001; i >= 0; i--) {
+	for (int i = 10100; i >= 0; i--) {
 		if (Positive.arr[i] > Negative.arr[i]) {
 			integer = '+';
 			same = 0;
@@ -71,23 +71,59 @@ int main() {
 
 	// 부호에 따라 절댓값 큰 것 - 절댓값 작은 것
 	if (integer == '+') {
-		for (int i = 10001; i >= 0; i--) {
+		for (int i = 10100; i >= 0; i--) {
 			Total.arr[i] = (Positive.arr[i] - Negative.arr[i]);
 		}
 	}
 	else if (integer == '-') {
-		for (int i = 10001; i >= 0; i--) {
+		for (int i = 10100; i >= 0; i--) {
 			Total.arr[i] = (Negative.arr[i] - Positive.arr[i]);
 		}
 	}
 
 	// Total 캐리 내림
-	for (int i = 10001; i > 0; i--) {
+	for (int i = 1; i < 10101; i++) {
 		if (Total.arr[i - 1] < 0) {
 			Total.arr[i]--;
 			Total.arr[i - 1] += 10;
 		}
 	}
+
+	// Total 캐리 올림
+	for (int i = 0; i < 10101; i++) {
+		if (Total.arr[i] / 10 > 0) {
+			Total.arr[i + 1] += (Total.arr[i] / 10);
+			Total.arr[i] %= 10;
+		}
+	}
+
+	/*
+	printf("\nPositive : ");
+	int zeroChecker = 0;
+	for (int i = 10100; i >= 0; i--) {
+		if (
+			Positive.arr[i] != 0) {
+			zeroChecker++;
+		}
+
+		if (zeroChecker > 0) {
+			printf("%d", Positive.arr[i]);
+		}
+	}
+
+	printf("\nNegative : ");
+	zeroChecker = 0;
+	for (int i = 10100; i >= 0; i--) {
+		if (Negative.arr[i] != 0) {
+			zeroChecker++;
+		}
+
+		if (zeroChecker > 0) {
+			printf("%d", Negative.arr[i]);
+		}
+	}
+	printf("\nTotal : ");
+	*/
 
 	// 0이 아니면 
 	if (same == 0) {
@@ -99,7 +135,7 @@ int main() {
 
 		// 앞의 0들은 출력 X
 		int zeroChecker = 0;
-		for (int i = 10001; i >= 0; i--) {
+		for (int i = 10100; i >= 0; i--) {
 			if (Total.arr[i] != 0) {
 				zeroChecker++;
 			}
