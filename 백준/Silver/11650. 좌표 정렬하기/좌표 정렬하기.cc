f6d93c9d83a8bec924a3a074a6eef1eb8c84dc49
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-05-10 01:29:47 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 120 ms, Memory: 2688 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// 노드 구조체 define
+typedef struct Node {
+	int x;
+	int y;
+} Node;
+
+// <algorithm> 라이브러리 내장 구조체 정렬 sort() 사용에 필요한 compare 함수
+bool compare(const Node& node1, const Node& node2) {
+	if (node1.x == node2.x) {
+		return (node1.y < node2.y);
+	}
+	else {
+		return (node1.x < node2.x);
+	}
+}
+
+int main() {
+	cin.tie(NULL);
+
+	// 테케 개수
+	int testCase;
+	cin >> testCase;
+	
+	// 노드 배열
+	Node nodeArr[101010];
+	for (int i = 0; i < testCase; i++) {
+		cin >> nodeArr[i].x;
+		cin >> nodeArr[i].y;
+	}
+
+	// 노드 정렬
+	sort(nodeArr, nodeArr + testCase, compare);
+	
+	// 정렬된 노드 출력
+	for (int i = 0; i < testCase; i++) {
+		cout << nodeArr[i].x << " " << nodeArr[i].y << "\n";
+	}
+
+	return 0;
+}
