8aa4573b94732ad62b67ba4a8ea178c77bee5b58
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-05-13 02:27:50 +0900

    [Bronze V] Title: Rectangles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < b; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
