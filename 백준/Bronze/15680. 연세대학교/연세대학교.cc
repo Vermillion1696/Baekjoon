b64150da64264bdc727a54f145c4a4330538dfe9
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-05-13 02:29:15 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	if (n == 0) {
+		cout << "YONSEI";
+		return 0;
+	}
+	else {
+		cout << "Leading the Way to the Future";
+		return 0;
+	}
+}
