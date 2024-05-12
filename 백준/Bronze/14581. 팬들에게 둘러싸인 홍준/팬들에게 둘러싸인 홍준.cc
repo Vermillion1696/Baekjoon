1bca5d14b26d1c401e6d9f8fe1030c7445a524e1
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-05-13 02:14:17 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name;
+	cin >> name;
+	cout << ":fan::fan::fan:" << endl;
+	cout << ":fan::" << name << "::fan:" << endl;
+	cout << ":fan::fan::fan:" << endl;
+	return 0;
+}
