0e351589cad042f783f283493cbc23067ff42681
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-05-13 12:47:39 +0900

    [Bronze V] Title: Serca, Time: 160 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int times;
+	cin >> times;
+
+	for (int i = 0; i < times; i++) {
+		cout << " @@@   @@@ " << "\n";
+		cout << "@   @ @   @" << "\n";
+		cout << "@    @    @" << "\n";
+		cout << "@         @" << "\n";
+		cout << " @       @ " << "\n";
+		cout << "  @     @  " << "\n";
+		cout << "   @   @   " << "\n";
+		cout << "    @ @    " << "\n";
+		cout << "     @     " << "\n";
+	}
+
+	return 0;
+}
