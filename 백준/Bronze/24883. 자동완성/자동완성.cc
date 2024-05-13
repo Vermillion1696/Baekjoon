0f59f6eb72ea51ba1f053c7a7b712f43b2d3166d
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-05-13 10:21:43 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char temp;
+	cin >> temp;
+
+	if (temp == 'N' || temp == 'n') {
+		cout << "Naver D2";
+	}
+	else {
+		cout << "Naver Whale";
+	}
+
+	return 0;
+}
