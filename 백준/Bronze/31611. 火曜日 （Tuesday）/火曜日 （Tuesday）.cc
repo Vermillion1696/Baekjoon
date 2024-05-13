7b287c20d75490b1075ab7050a0e45888fa63daa
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-05-14 01:11:52 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    if (n % 7 == 2) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+
+    return 0;
+}
