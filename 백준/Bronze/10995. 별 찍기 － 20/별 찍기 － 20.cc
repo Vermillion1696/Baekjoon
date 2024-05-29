f04bda6d9bcef8b222fc1d3a04341c7c3f2cda7c
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-05-29 21:12:01 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 == 1) {
+			for (int j = 1; j <= n; j++) {
+				cout << "* ";
+			}
+		}
+		else {
+			for (int j = 1; j <= n; j++) {
+				cout << " *";
+			}
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
