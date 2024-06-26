ae4b4b682edd1e6acabaa8946928752554faf819
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-06-26 21:21:12 +0900

    [Gold IV] Title: 불!, Time: 124 ms, Memory: 13768 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,166 +1,214 @@
 #include <iostream>
 #include <queue>
 #include <utility>
-#include <tuple>
 using namespace std;
-
 #define Y first
 #define X second
 #define MAX 1001
 
-// 0 : 길
-// 1 : 지훈 (또는 지나온 길)
-// 2 : 벽
-// 3 : 불
-int map[MAX][MAX];
-const int dx[4] = { 1,0,-1,0 };
-const int dy[4] = { 0,1,0,-1 };
+// map[z][~][~]
+// z == 0 : map
+// z == 1 : 지훈이가 칸에 도달하는 데 걸린 시간 저장
+// z == 2 : 불이 칸에 도달하는 데 걸린 시간
+#define B 0
+#define P 1
+#define F 2
+int map[3][MAX][MAX];
+int dy[4] = { 0,0,1,-1 };
+int dx[4] = { 1,-1,0,0 };
 
-// 디버깅용 맵 출력 함수
-void mapPrint(const int& column, const int& row) {
-    for (int i = 1; i <= row; i++) {
-        for (int j = 1; j <= column; j++) {
-            cout << map[i][j] << " ";
-        }
-        cout << "\n";
-    }
-    return;
+// map[z][~][~] 에 대해 z : B 이면...
+// 0 : 길
+// 1 : 지훈
+// 2 : 불
+// 3 : 벽
+// map[z][~][~] 에 대해 z : P or F 이면...
+// -1 : 못 가는 곳
+// 0 : 안 간 곳
+// + : 간 곳 (시간)
+
+// 맵 초기 세팅 함수
+void fillMAP(const int& row, const int& column) {
+	char c;
+	for (int i = 1; i <= row; i++) {
+		for (int j = 1; j <= column; j++) {
+			cin >> c;
+			if (c == '.') {
+				map[B][i][j] = 0;
+			}
+			else if (c == 'J') {
+				map[B][i][j] = 1;
+				map[P][i][j] = 0;
+			}
+			else if (c == 'F') {
+				map[B][i][j] = 2;
+				map[P][i][j] = -1;
+				map[F][i][j] = 0;
+			}
+			else {
+				map[B][i][j] = 3;
+				map[P][i][j] = -1;
+				map[F][i][j] = -1;
+			}
+		}
+	}
 }
 
-int BFS(const int& column, const int& row) {
+// 디버깅용 출력 함수
+void printMAP(const int& dimension, const int& row, const int& column) {
+	cout << "\n";
+	for (int i = 1; i <= row; i++) {
+		for (int j = 1; j <= column; j++) {
+			cout << map[dimension][i][j];
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+}
 
-    // 시작점 찾기
-    queue<pair<int, int>> fireSTART;
-    pair<int, int> JihunSTART;
-    for (int i = 1; i <= row; i++) {
-        for (int j = 1; j <= column; j++) {
-            if (map[i][j] == 3) {
-                fireSTART.push({ i,j });
-            }
-            else if (map[i][j] == 1) {
-                JihunSTART = { i,j };
-            }
-        }
-    }
+// player BFS
+void PBFS(queue<pair<int, int>> &q, const int& row, const int& column) {
+	while (!q.empty()) {
+		int y, x;
+		y = q.front().first;
+		x = q.front().second;
+		q.pop();
 
-    int time = 0;
-    queue<tuple<int, int, int>> QJ;
-    queue<tuple<int, int, int>> QF;
-    QJ.push(make_tuple(JihunSTART.Y, JihunSTART.X, time));
-    while (!fireSTART.empty()) {
-        QF.push(make_tuple(fireSTART.front().Y, fireSTART.front().X, time));
-        fireSTART.pop();
-    }
+		for (int dir = 0; dir < 4; dir++) {
+			int ny = y + dy[dir];
+			int nx = x + dx[dir];
 
-    while (!QJ.empty() || !QF.empty()) {
-        time++;
+			if (ny <= 0 || ny > row || nx <= 0 || nx > column) {
+				continue;
+			}
+			// 길만 잡아먹는다
+			if (map[P][ny][nx] != 0 || map[B][ny][nx] != 0) {
+				continue;
+			}
 
-        // 불이 먼저 번진다
-        int fireSize = QF.size();
-        while (fireSize--) {
-            int fy, fx, ft;
-            tie(fy, fx, ft) = QF.front();
-            QF.pop();
+			map[P][ny][nx] = map[P][y][x] + 1;
+			q.push(make_pair(ny, nx));
+		}
+	}
+	return;
+}
 
-            for (int dir = 0; dir < 4; dir++) {
-                int ny = fy + dy[dir];
-                int nx = fx + dx[dir];
+// fire BFS
+void FBFS(queue<pair<int, int>>& q, const int& row, const int& column) {
+	while (!q.empty()) {
+		int y, x;
+		y = q.front().first;
+		x = q.front().second;
+		q.pop();
 
-                if (nx <= 0 || nx > column || ny <= 0 || ny > row) {
-                    continue;
-                }
-                if (map[ny][nx] == 0 || map[ny][nx] == 1) {
-                    map[ny][nx] = 3;
-                    QF.push(make_tuple(ny, nx, time));
-                }
-            }
-        }
+		for (int dir = 0; dir < 4; dir++) {
+			int ny = y + dy[dir];
+			int nx = x + dx[dir];
 
-        // 지훈이 이동한다
-        int jihunSize = QJ.size();
-        while (jihunSize--) {
-            int jy, jx, jt;
-            tie(jy, jx, jt) = QJ.front();
-            QJ.pop();
+			if (ny <= 0 || ny > row || nx <= 0 || nx > column) {
+				continue;
+			}
+			// 불 또는 벽이면 continue; (길, 사람은 잡아먹는다)
+			if (map[F][ny][nx] != 0 || map[B][ny][nx] == 2 || map[B][ny][nx] == 3) {
+				continue;
+			}
 
-            for (int dir = 0; dir < 4; dir++) {
-                int ny = jy + dy[dir];
-                int nx = jx + dx[dir];
+			map[F][ny][nx] = map[F][y][x] + 1;
+			q.push(make_pair(ny, nx));
+		}
+	}
+	return;
+}
 
-                if (nx <= 0 || nx > column || ny <= 0 || ny > row) {
-                    // 지훈이 맵의 가장자리에 도착하면 탈출
-                    return time;
-                }
-                if (map[ny][nx] == 0) {
-                    map[ny][nx] = 1;
-                    QJ.push(make_tuple(ny, nx, time));
-                }
-            }
-        }
-
-        // 지훈의 위치가 가장자리에 있는지 다시 검사
-        for (int i = 1; i <= column; i++) {
-            if (map[1][i] == 1 || map[row][i] == 1) {
-                return time + 1;
-            }
-        }
-        for (int i = 1; i <= row; i++) {
-            if (map[i][1] == 1 || map[i][column] == 1) {
-                return time + 1;
-            }
-        }
-
-        // 탈출 조건 검사
-        // 패배 조건 : 지훈(1)이 어느 곳에도 없음
-        bool available = false;
-        for (int i = 1; i <= row; i++) {
-            for (int j = 1; j <= column; j++) {
-                if (map[i][j] == 1) {
-                    available = true;
-                    goto forOUT;
-                }
-            }
-        }
-    forOUT:
-        if (!available) {
-            return -1;
-        }
-    }
-
-    // 탈출 조건을 못 타고 while문을 나가면 불가능이다
-    return -1;
+// 시작점이 탈출구인지 반환하는 함수
+bool isBoundary(const int& row, const int& column) {
+	for (int i = 1; i <= row; i++) {
+		if (map[B][i][1] == 1 || map[B][i][column] == 1) {
+			return 1;
+		}
+	}
+	for (int j = 1; j <= column; j++) {
+		if (map[B][1][j] == 1 || map[B][row][j] == 1) {
+			return 1;
+		}
+	}
+	return 0;
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+	int row, column;
+	cin >> row >> column;
 
-    int column, row;
-    cin >> row >> column;
+	fillMAP(row, column);
 
-    char c;
-    for (int i = 1; i <= row; i++) {
-        for (int j = 1; j <= column; j++) {
-            cin >> c;
-            if (c == '#') {
-                map[i][j] = 2;
-            }
-            else if (c == 'F') {
-                map[i][j] = 3;
-            }
-            else if (c == '.') {
-                map[i][j] = 0;
-            }
-            else {
-                map[i][j] = 1;
-            }
-        }
-    }
+	// 시작점 입력 받아 각각 큐에 넣기
+	queue<pair<int, int>> PQ;
+	queue<pair<int, int>> FQ;
+	for (int i = 1; i <= row; i++) {
+		for (int j = 1; j <= column; j++) {
+			if (map[B][i][j] == 1) {
+				PQ.push(make_pair(i, j));
+			}
+			else if (map[B][i][j] == 2) {
+				FQ.push(make_pair(i, j));
+			}
+		}
+	}
 
-    int time = BFS(column, row);
+	// Player에 대한 BFS, Fire에 대한 BFS를 각각 돌림
+	PBFS(PQ, row, column);
+	FBFS(FQ, row, column);
 
-    time > 0 ? cout << time : cout << "IMPOSSIBLE";
+	// BFS를 하고도 0이 남아있으면 고립된 위치이므로 (불이 방문 불가), -1로 바꿔야 한다.
+	// P : map[P][i][j] == 0 (방문 가능) 인데 map[B][i][j] != 1 (시작점이 아니면) -> -1로
+	// F : map[F][i][j] == 0 (방문 가능) 인데 map[B][i][j] != 2 (시작점이 아니면) -> -1로
+	for (int i = 1; i <= row; i++) {
+		for (int j = 1; j <= column; j++) {
+			if (map[F][i][j] == 0 && map[B][i][j] != 2) {
+				map[F][i][j] = -1;
+			}
+			if (map[P][i][j] == 0 && map[B][i][j] != 1) {
+				map[P][i][j] = -1;
+			}
+		}
+	}
 
-    return 0;
+	//printMAP(F, row, column);
+	//printMAP(P, row, column);
+	//printMAP(B, row, column);
+	
+	// 가장자리까지 가는 최소 시간 찾기
+	int minTIME = MAX * MAX;
+	for (int i = 1; i <= row; i++) {
+		// 길이면서 도달가능 + (플레이어 도달 시간 < 불 도달 시간) 또는 (불 도착 불가) + (탈출구 도달 시간 < 최소 시간)   
+		if (map[B][i][1] == 0 && map[P][i][1] != -1 && (map[P][i][1] < map[F][i][1] || map[F][i][1] == -1) && map[P][i][1] < minTIME) {
+			minTIME = map[P][i][1];
+		}
+		if (map[B][i][column] == 0 && map[P][i][column] != -1 && (map[P][i][column] < map[F][i][column] || map[F][i][column] == -1) && map[P][i][column] < minTIME) {
+			minTIME = map[P][i][column];
+		}
+	}
+	for (int j = 1; j <= column; j++) {
+		if (map[B][1][j] == 0 && map[P][1][j] != -1 && (map[P][1][j] < map[F][1][j] || map[F][1][j] == -1) && map[P][1][j] < minTIME) {
+			minTIME = map[P][1][j];
+		}
+		if (map[B][row][j] == 0 && map[P][row][j] != -1 && (map[P][row][j] < map[F][row][j] || map[F][row][j] == -1) && map[P][row][j] < minTIME) {
+			minTIME = map[P][row][j];
+		}
+	}
+
+	// 가장자리가 시작점이면
+	if (isBoundary(row, column)) {
+		cout << 1;
+	}
+	// 불가능 판정이면
+	else if (minTIME == MAX * MAX) {
+		cout << "IMPOSSIBLE";
+	}
+	// 가장자리까지 가는 최소 시간이므로 +1
+	else {
+		cout << minTIME + 1;
+	}
+
+	return 0;
 }
