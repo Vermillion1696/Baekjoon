62c3bd98b403a48e75dd8e4bc90254cfc4ca8589
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-07-11 22:49:47 +0900

    [Bronze IV] Title: 히스토그램, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int tc;
+	cin >> tc;
+
+	while (tc--) {
+		int value;
+		cin >> value;
+
+		while (value--) cout << "=";
+		cout << "\n";
+	}
+
+	return 0;
+}
