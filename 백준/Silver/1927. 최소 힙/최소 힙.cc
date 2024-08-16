ca661baef1da315e2cc1e89cbbe1c5358c94d320
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-08-16 11:47:51 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#define faster_output cin.tie(0); ios::sync_with_stdio(0);
+using namespace std;
+
+int main() {
+	faster_output
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+	int command_cnt, value;
+	cin >> command_cnt;
+
+	while (command_cnt--) {
+		cin >> value;
+
+		if (value > 0) pq.push(value);
+		else {
+			if (pq.empty()) cout << 0 << "\n";
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+	}
+
+	return 0;
+}
