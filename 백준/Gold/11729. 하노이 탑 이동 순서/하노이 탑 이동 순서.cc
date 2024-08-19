ce47be1b596e51855b0d0f1d1d38da456d5c63aa
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-08-19 16:01:06 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 104 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -1,36 +1,39 @@
 #include <iostream>
+#define fast_output cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
 using namespace std;
-int x, y;
 
-// 움직임 출력
-void move(const int a, const int b) {
-	cout << a << " " << b << "\n";
-	return;
-}
+// 인자 순서 : 시작 위치, 옮길 위치, 경유 지점, 개수
+void hanoi(int from, int to, int via, long long int cnt) {
 
-// (1) : Base
-// (1)2(1)
-// (121)3(121)
-// (1213121)4(1213121)
-// ...
-void hanoi(int n, int x, int y, int z) {
-	if (n > 0) {
-		hanoi(n - 1, x, z, y);
-		move(x, y);
-		hanoi(n - 1, z, y, x);
+	// 이동 개수가 1개인 경우
+	if (cnt == 1) {
+		cout << from << " " << to << "\n";
 	}
-	return;
+	// 이동 개수가 1개 이상인 경우
+	else {
+
+		// cnt - 1개를 from -> via
+		hanoi(from, via, to, cnt - 1);
+
+		// 1개를 from -> to (제일 큰 것)
+		cout << from << " " << to << "\n";
+
+		// cnt - 1개를 via -> to
+		hanoi(via, to, from, cnt - 1);
+
+	}
 }
 
 int main() {
-	int n;
-	long long int total = 1;
+	fast_output;
+
+	long long n;
+	int x = 1, y = 2, z = 3;
 	cin >> n;
 
-	for (int i = 1; i <= n; i++) {
-		total *= 2;
-	}
+	// 2^n - 1을 비트마스킹으로 표현
+	cout << (1 << n) - 1 << "\n";
+	if (n <= 20) hanoi(x, z, y, n);
 
-	cout << total - 1 << endl;
-	hanoi(n, 1, 3, 2);
+	return 0;
 }
