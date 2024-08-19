de1d40a5a5ee18c390bf37b13ebdc8ba45149891
Vermillion1696 <163641488+Vermillion1696@users.noreply.github.com>
2024-08-19 12:22:18 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 112 ms, Memory: 17332 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.cc
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.cc
@@ -4,6 +4,7 @@
 #include <vector>
 using namespace std;
 
+// 커스텀 비교 인자
 bool compare(pair<string, int>& a, pair<string, int>& b) {
 	if (a.second != b.second) return a.second > b.second;
 	if (a.first.length() != b.first.length()) return a.first.length() > b.first.length();
@@ -16,18 +17,23 @@ int main() {
 	string str;
 	int words, min_length;
 	cin >> words >> min_length;
-
+    
+    // 단어 입력
 	while (words--) {
 		cin >> str;
+        
+        // 단어 길이가 최소 길이보다 작으면 입력 X
 		if (str.length() < min_length) continue;
 		map[str]++;
 	}
 	
+    // 벡터에 복사
 	vector<pair<string, int>> v;
 	for (auto iter = map.begin(); iter != map.end(); iter++) {
 		v.push_back({ iter->first, iter->second });
 	}
-
+    
+    // 기준에 맞게 정렬
 	sort(v.begin(), v.end(), compare);
 
 	for (auto iter = v.begin(); iter != v.end(); iter++) {
