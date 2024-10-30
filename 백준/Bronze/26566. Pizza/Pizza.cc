7ac67fa7222c4e64b16dcfb146275464e4999f08
김수오 (Su O Kim) <163641488+Vermillion1696@users.noreply.github.com>
2024-10-30 11:22:05 +0900

    [Bronze IV] Title: Pizza, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26566. Pizza/Pizza.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int tc;
+	cin >> tc;
+
+	while (tc--) {
+		double slice_area, whole_area;
+		int slice_price, whole_price;
+		double whole_radius;
+		
+		cin >> slice_area >> slice_price;
+		cin >> whole_radius >> whole_price;
+		whole_area = whole_radius * whole_radius * 3.141592;
+
+		if (whole_area / whole_price > slice_area / slice_price) {
+			cout << "Whole pizza" << endl;
+		}
+		else {
+			cout << "Slice of pizza" << endl;
+		}
+	}
+
+	return 0;
+}
