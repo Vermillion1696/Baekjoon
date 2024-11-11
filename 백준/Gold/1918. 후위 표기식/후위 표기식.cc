27209130ef1f7c8aedb2bec26ac1cd87ee7e6b1c
김수오 (Su O Kim) <163641488+Vermillion1696@users.noreply.github.com>
2024-11-12 00:15:27 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.cc
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -1,39 +1,5 @@
-// 2학년 2학기 자료구조 중간고사 대비겸 연습 문제
-/*
-하단 로직을 구현했음을 알립니다.
-
-while (unless there is no more input to read)
-
-- if the next input is a left parenthesis
-
-	→  push it onto the stack
-
-- else if the next input is a number or a variable
-
-	→ write it to the output
-
-- else if the next input is an operator
-
-	→ do { print the top operator and pop it } while (**none** of **these three conditions** are true…);
-
-	1. the stack becomes **empty**.
-	2. the top item on the stack is a **left parenthesis**.
-	3. the top item on the stack is an operator with **lower precedence than the input operator**.
-
-	→ push the input operator onto the stack.
-
-- else (the next input is a right parenthesis)
-
-	→ write the top item(operator) and pop it off the stack.
-
-	→ keep printing and popping until a left parenthesis is the top item on the stack and then pop the ‘(’.
-
-
-print and pop any remaining operators on the stack.
-*/
-
 #include <iostream>
-#include <stack>
+#include <stack>    
 using namespace std;
 
 // 연산자에 따른 우선순위 반환
@@ -45,7 +11,6 @@ int precedence(const char op) {
 }
 
 int main() {
-
 	stack<char> s;
 	string str;
 	cin >> str;
